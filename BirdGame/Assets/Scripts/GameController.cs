975ed0501daf396484caa960e4e8cdc70a57b5d2
Devid Chhaya <chhayadevid1993@gmail.com>
2020-11-20 19:11:26 +0530

    my first commit

---
+++ BirdGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public Rigidbody2D rb;
+    public float upForce;
+    public GameObject go;
+    public bool isDead;
+    public Text scoretext;
+    int score = 0;
+    public Animator anim;
+
+    void Awake()
+    {
+        //If we don't currently have a game control...
+        if (instance == null)
+            //...set this one to be it...
+            instance = this;
+        //...otherwise...
+        else if (instance != this)
+            //...destroy this one because it is a duplicate.
+            Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Bird Position
+                rb.velocity = Vector2.zero;
+                //Trigger for Bird Animate
+                anim.SetTrigger("Tap");
+                //AddForce to Bird for Fly.
+                rb.AddForce(new Vector2(0, upForce));
+            }
+        }
+        else if ((isDead == true) && Input.GetMouseButtonDown(0))
+        {
+            //When GameOver and Player Tap Mouse Button to Restart Game.
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("OnTriggerEnter2D Called");
+
+        //if (collision.tag == "ScoreTigger")
+        {
+            //score increment
+            score++;
+            Debug.Log("Collision Method Called");
+            //display score
+            scoretext.text = "SCORE : " + score;
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("Collision Method Called");
+        if (collision.collider.tag == "Ground" || collision.collider.tag == "Column")
+        {
+            anim.SetTrigger("Die");
+            isDead = true;
+            go.SetActive(true);
+        }
+    }
+}
---
+++ BirdGame/Assets/Scripts/ScrollObject.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+    //This stores a reference to the collider attached to the Ground.
+    private float groundHorizontalLength;
+    Rigidbody2D rb;
+    BoxCollider2D rb2;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //Start the object moving.
+        rb.velocity = new Vector2(-1f, 0);
+    }
+           //A float to store the x-axis length of the collider2D attached to the Ground GameObject.
+
+    //Awake is called before Start.
+    private void Awake()
+    {
+        //Get and store a reference to the collider2D attached to Ground.
+        rb2 = GetComponent<BoxCollider2D>();
+        //Store the size of the collider along the x axis (its length in units).
+        groundHorizontalLength = rb2.size.x;
+    }
+
+    //Update runs once per frame
+    private void Update()
+    {
+        //Check if the difference along the x axis between the main Camera and the position of the object this is attached to is greater than groundHorizontalLength.
+        if (transform.position.x < -groundHorizontalLength)
+        {
+            //If true, this means this object is no longer visible and we can safely move it forward to be re-used.
+            RepositionBackground();
+        }
+
+        if(GameController.instance.isDead==true)
+            rb.velocity = Vector2.zero;
+    }
+
+    //Moves the object this script is attached to right in order to create our looping background effect.
+    private void RepositionBackground()
+    {
+        //This is how far to the right we will move our background object, in this case, twice its length. This will position it directly to the right of the currently visible background object.
+        Vector2 groundOffSet = new Vector2(groundHorizontalLength * 2f, 0);
+
+        //Move this object from it's position offscreen, behind the player, to the new position off-camera in front of the player.
+        transform.position = (Vector2)transform.position + groundOffSet;
+    }
+}
